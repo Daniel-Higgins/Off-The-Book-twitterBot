73783f62a49f416a60b204a02797fd477ee0a19c
Daniel Higgins <dannyhiggs5@gmail.com>
2022-04-18 20:42:38 -0400

    Create bookDictionary.py

---
+++ bookDictionary.py
@@ -0,0 +1,7 @@
+websites = {"Caesars":"https://www.williamhill.com/",
+           "BetMGM":"https://sports.betmgm.com/en/sports",
+    "DraftKings":"https://sportsbook.draftkings.com/sportsbetting",
+           "Fanduel":"https://sportsbook.fanduel.com/",
+           "PointsBet":"https://pointsbet.com/",
+           "Barstool":"https://www.barstoolsportsbook.com/",
+           "WynnBet":"https://www.wynnbet.com/"}
